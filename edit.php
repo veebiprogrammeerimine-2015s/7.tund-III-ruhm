21af59cb7f086757cb3d8f80975bc443cc03681c
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-14 11:49:49 +0300

    edit saving in db

--- edit.php
+++ edit.php
@@ -2,6 +2,17 @@
 	
 	require_once("edit_functions.php");
 	
+		
+	if(isset($_POST["update_plate"])){
+		// vajutas salvesta nuppu
+		// numberplate ja color tulevad vormist
+		// aga id varjatud väljast
+		updateCar($_POST["id"], $_POST["number_plate"], $_POST["color"]);
+		
+	}
+	
+	
+	
 	//edit.php
 	// aadressireal on ?edit_id siis trükin välja selle väärtuse
 	if(isset($_GET["edit_id"])){
@@ -24,17 +35,16 @@
 		header("Location: table.php");
 		
 	}
-	
-
 
 ?>
 
 <h2>Muuda autonumbrimärkki</h2>
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
+	<input type="hidden" name="id" value="<?=$_GET["edit_id"];?>">
 	<label for="number_plate" >Auto numbrimärk</label><br>
-	<input id="number_plate" name="number_plate" type="text" value=""><br><br>
+	<input id="number_plate" name="number_plate" type="text" value="<?=$car->number_plate;?>"><br><br>
 	<label for="color">Värv</label><br>
-	<input id="color" name="color" type="text" value=""><br><br>
-	<input type="submit" name="add_plate" value="Salvesta">
+	<input id="color" name="color" type="text" value="<?=$car->color;?>"><br><br>
+	<input type="submit" name="update_plate" value="Salvesta">
 </form>
 
--- edit_functions.php
+++ edit_functions.php
@@ -37,5 +37,24 @@
 
 		
 	}
+	
+	
+	
+	function updateCar($id, $number_plate, $color){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+
+		$stmt = $mysqli->prepare("UPDATE car_plates SET number_plate=?, color=? WHERE id=?");
+		$stmt->bind_param("ssi", $number_plate, $color, $id);
+		if($stmt->execute()){
+			// sai uuendatud
+			// kustutame aadressirea tühjaks
+			header("Location: table.php");
+			
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+	}
 
 ?>
